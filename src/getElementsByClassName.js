68ee2151ec133cb5e58ab9f580a85b39b703f791
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-11 00:36:57 -0700

    Got getElements to push wrong elements to result variable

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,60 @@
 //   return document.getElementsByClassName(className);
 // };
 
+
+
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
   // your code here
+  // Does stuff to get all elements that have the same class name
+  // Goes into 
+
+  // Base Case
+  // If there are no children of current element, return element
+  // if (className.children.length === 0) {
+  //   return 
+  // }
+
+// Look to see if childNodes exist
+// if childNodes[className] === className
+// return nodeName
+
+// if ()
+
+  var nodes = document.childNodes
+  var result = [];
+
+  // If there are childNodes
+  // Look at each childNode and see if className is target
+  nodes.forEach(function(node) {
+  	console.log(node.childNodes.length)
+  	console.log(node)
+  	console.log(node.className)
+
+  	if (node.className = className) {
+  	  result.push(node.nodeName);
+  	}
+  	if (node.childNodes.length > 0) {
+
+  	}
+  	if (node.classList) {
+  	  console.log('hi')
+  	}
+
+  });
+
+
+  // var classList = body.classList;
+
+  // var result = classList.map(function (element) {
+  // 	element.classList.forEach(function (item) {
+  // 	  if (item) {
+  // 		return 'hi'
+  // 	  };
+  //   });
+  // });
+  	
+console.log("this className: ", document.childNodes)
+console.log("this result: ", result)
 };
